072df27ae81125dc63abc9bdae7e4672737f4ece
Nyakio Mburu <nyakio.mburu@gmail.com>
2018-09-14 17:35:35 +0300

    adds a background and attempt to justify content asides from those pesky bullet points

--- js/scripts.js
+++ js/scripts.js
@@ -9,10 +9,10 @@ var sideC = .getElementById('SideC')
 for ( if(sideA == sideB == sideC){
     alert("Equilateral")
   };
-else if(sideA == sideB ||sideA == sideC || sideB == sideC){
+else if(sideA == sideB || sideA == sideC || sideB == sideC){
     alert("Isosceles")};
   };
 else if (sideA + sideB < SideC || sideC + sideA < SideB || sideC + sideB < SideA) {
-    alert("")
+    alert("Not a triangle")
 }
 })
