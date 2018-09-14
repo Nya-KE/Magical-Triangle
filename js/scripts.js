d33d644a984d7b5556b1fa5462914f6da1642578
Nyakio Mburu <nyakio.mburu@gmail.com>
2018-09-14 18:07:44 +0300

    hides responses and perfects triangle identification - but only with alerts

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,23 @@
 //document.getElementByClass('RESPONSE')
 //document.getElementByClass('MAGICAns')
 //document.getElementByClass('FIELD')
+function pressTo() {
 
-var sideA = .getElementById('SideA')
-var sideB = .getElementById('SideB')
-var sideC = .getElementById('SideC')
+    var sideA = document.getElementById('SideA').value
+    var sideB = document.getElementById('SideB').value
+    var sideC = document.getElementById('SideC').value
 
-for ( if(sideA == sideB == sideC){
-    alert("Equilateral")
-  };
-else if(sideA == sideB || sideA == sideC || sideB == sideC){
-    alert("Isosceles")};
-  };
-else if (sideA + sideB < SideC || sideC + sideA < SideB || sideC + sideB < SideA) {
-    alert("Not a triangle")
-}
-})
+    if(sideA == sideB && sideA == sideC){
+        alert("Equilateral")
+        document.getElementByClass('tri').style.display = "inline"//causes default to something visible ovverriding displaynone in CSS;
+    } else if(sideA == sideB || sideA == sideC || sideB == sideC){
+        alert("Isosceles")
+    } else if (sideA + sideB < SideC || sideC + sideA < SideB || sideC + sideB < SideA) {
+        alert("Not a triangle")
+
+    }
+      else {alert("Scalene")
+    }
+
+
+}//ends 'pressTo'function
