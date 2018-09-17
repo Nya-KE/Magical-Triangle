9dfe4b67e57fb0626fec353cef056bf9fefda8c5
Nyakio Mburu <nyakio.mburu@gmail.com>
2018-09-17 10:56:08 +0300

    Got messages to present on HTML finally, although scalene is giving trouble ref. to JS line 40. It's been a goodn one Nairobi\!

--- js/scripts.js
+++ js/scripts.js
@@ -8,16 +8,37 @@ function pressTo() {
     var sideA = document.getElementById('SideA').value
     var sideB = document.getElementById('SideB').value
     var sideC = document.getElementById('SideC').value
+    var tri = document.getElementById('tri')
+
+    var triangleSides = []
+     triangleSides.push(sideA)
+     triangleSides.push(sideB)
+     triangleSides.push(sideC)
+     console.log(triangleSides)
+
+    /*
+    console.log(triangle.push(){
+      triangle.charAt[0] ;
+      triabglechar[1] = sideB;
+      char[2] = sideC;)
+    })*/
 
     if(sideA == sideB && sideA == sideC){
         alert("Equilateral")
-        return document.getElementByClass('tri').style.display = "inline"//causes default to JavaScript style for value 'tri' located in HTML, overriding display none ine CSS
+        //method 1 jquery?
+        tri.style.display = "block";
+        //method 2 hide and show toggle attempt which causes default to JavaScript style for value 'tri' located in HTML, overriding display none ine CSS
+        //document.getElementsByClassName('tri').style.display = "block"
     } else if(sideA == sideB || sideA == sideC || sideB == sideC){
+        //note varied code direct ÍD or js variable for experimenting with concise code
         alert("Isosceles")
-    } else if (sideA + sideB >= SideC || sideC + sideA >= SideB || sideC + sideB >= SideA) {
-        alert("Scalene")
-    }
-      else {alert("Not a Triangle")
+        document.getElementById('tri2').style.display = "block"
+    } else if (sideA + sideB <= SideC || sideC + sideA <= SideB || sideC + sideB <= SideA) {
+        alert("Not a Triangle")
+        document.getElementById('not').style.display = "block"
+    } else if (SideA !== sideB && sideA !== sideC && sideB !== sideC) {
+        alert("Scalene") //YET TO BE CORRECTLY RESOLVED AS EVEN VALUES 4,7,9 which defy previous if statement did not result to 'Scalene'alert
+        document.getElementById('tri2').style.display = "block"
     }
 
 
